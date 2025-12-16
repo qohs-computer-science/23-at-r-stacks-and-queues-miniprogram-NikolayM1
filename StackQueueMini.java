91abf456ccb5bd66e22014cd1490f6b8f936b406
Nikolay <nmerenko07@gmail.com>
2025-12-16 17:47:46 +0000

    Tuesday coding

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -3,8 +3,38 @@ import java.util.Stack;
 import java.util.Queue;
 import java.util.LinkedList;
 
+
 class StackQueueMini {
   public static void main(String[] args) {
+    
+  Stack<Integer> myStack = new Stack<>();
+    try (Scanner temp = new Scanner(System.in)) {
+            
+  System.out.println("Please enter your first whole number:");
+    int whole1 = temp.nextInt();
+      myStack.push(whole1);
+        
+  System.out.println("Please enter your second whole number:");
+    int whole2 = temp.nextInt();
+      myStack.push(whole2);
+                            
+  System.out.println("Please enter your third whole number:");
+    int whole3 = temp.nextInt();
+      myStack.push(whole3);
+                                    
+  System.out.println("Please enter your fourth whole number:");
+    int whole4 = temp.nextInt();
+      myStack.push(whole4);
+        
+  System.out.println("Please enter your fifth whole number:");
+    int whole5 = temp.nextInt();
+      myStack.push(whole5);
+  }
 
-  } // end main
-} // end class
+  while (!myStack.isEmpty()) {
+    System.out.println("Here is what is in the stack:" + myStack.pop());
+  }
+
+    } //end main
+  } //end class
+  
